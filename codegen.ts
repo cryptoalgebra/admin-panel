20df5590bc1b61ec756b65cd8517111cfab4d350
lilchizh <xlilchizhx@gmail.com>
2025-04-02 16:01:56 +0300

    bolt deployment

--- codegen.ts
+++ codegen.ts
@@ -4,9 +4,9 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: [
-    'https://api.goldsky.com/api/public/project_cm8gyxv0x02qv01uphvy69ey6/subgraphs/base-sepolia-analytics/1.0.0/gn',
-    'https://api.goldsky.com/api/public/project_cm8gyxv0x02qv01uphvy69ey6/subgraphs/base-sepolia-blocks/1.0.0/gn',
-    'https://api.goldsky.com/api/public/project_cm8gyxv0x02qv01uphvy69ey6/subgraphs/base-sepolia-farms/1.0.0/gn',
+    'https://api.studio.thegraph.com/query/43776/bolt-analytics/v0.0.2',
+    'https://api.studio.thegraph.com/query/43776/megaeth-blocks/v0.0.1',
+    'https://api.studio.thegraph.com/query/43776/bolt-farming/v0.0.1',
   ],
   documents: "src/graphql/queries/!(*.d).{ts,tsx}",
   generates: {
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -6,38 +6,41 @@ import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { publicProvider } from 'wagmi/providers/public'
 import { defineChain } from 'viem'
 
-export const baseSepolia = defineChain({
-  id: 84532,
-  network: 'base-sepolia',
-  name: 'Base Sepolia',
-  nativeCurrency: { name: 'Sepolia Ether', symbol: 'ETH', decimals: 18 },
+export const megaethTestnet = defineChain({
+  id: 6342,
+  name: 'MegaETH Testnet',
+  network: 'megaeth-testnet',
+  nativeCurrency: {
+    name: 'MegaETH Testnet Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
   rpcUrls: {
-    public: {
-      http: ['https://sepolia.base.org'],
-    },
     default: {
-      http: ['https://sepolia.base.org'],
+      http: ['https://carrot.megaeth.com/rpc'],
+    },
+    public: {
+      http: ['https://carrot.megaeth.com/rpc'],
     },
   },
   blockExplorers: {
     default: {
-      name: 'Basescan',
-      url: 'https://sepolia.basescan.org',
-      apiUrl: 'https://api-sepolia.basescan.org/api',
+      name: 'MegaETH Testnet Explorer',
+      url: 'https://www.megaexplorer.xyz/',
     },
   },
   contracts: {
     multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 1059647,
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
     },
   },
   testnet: true,
 })
 
 
+
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [baseSepolia],
+  [megaethTestnet],
   [
     publicProvider(),
   ],
