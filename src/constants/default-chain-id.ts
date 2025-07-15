797e53c0eb8862e6070cf01195fa366beea28670
Kirill S <skdamn@outlook.com>
2025-07-16 00:04:20 +0300

    hyperevm mainnet deploy

--- src/constants/default-chain-id.ts
+++ src/constants/default-chain-id.ts
@@ -3,4 +3,4 @@ import { ChainId } from "@cryptoalgebra/integral-sdk";
 export const DEFAULT_CHAIN_NAME = "HyperEVM";
 export const DEFAULT_NATIVE_SYMBOL = "HYPE";
 export const DEFAULT_NATIVE_NAME = "HYPE";
-export const DEFAULT_CHAIN_ID = ChainId.HyperEvmTestnet;
+export const DEFAULT_CHAIN_ID = ChainId.HyperEvm;
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -6,30 +6,33 @@ import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
 import { publicProvider } from 'wagmi/providers/public'
 
-const hyperEvmTestnet = defineChain({
-  id: 998,
-  network: "hyperEVM-testnet",
-  name: "Hyperliquid EVM Testnet",
-  nativeCurrency: { name: "HYPE", symbol: "HYPE", decimals: 18 },
-  rpcUrls: {
-      default: {
-          http: ["https://998.rpc.thirdweb.com/c12d40e08559e44221f53ce0a23b7e67"],
-      },
-      public: {
-        http: ["https://998.rpc.thirdweb.com/c12d40e08559e44221f53ce0a23b7e67"],
+const hyperevmChain = defineChain({
+    id: 999,
+    network: "hyperevm",
+    name: "HyperEVM",
+    nativeCurrency: { name: "HYPE", symbol: "HYPE", decimals: 18 },
+    rpcUrls: {
+        default: {
+            http: ["https://rpc.hyperliquid.xyz/evm"],
+        },
+        public: {
+            http: ["https://rpc.hyperliquid.xyz/evm"],
+        },
     },
-  },
-  blockExplorers: {
-      default: {
-          name: "Purrsec",
-          url: "https://testnet.purrsec.com",
-      },
-  },
-  testnet: true,
+    blockExplorers: {
+        hyperScan: {
+            name: "HyperScan",
+            url: "https://hyperscan.com/",
+        },
+        default: {
+            name: "HyperEVMScan",
+            url: "https://hyperevmscan.io/",
+        },
+    },
 });
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [hyperEvmTestnet],
+  [hyperevmChain],
   [
     publicProvider(),
   ],
