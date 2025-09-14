0df4a6ee3205818f5ca377d4160e79edc9180bbb
damnnou <skdamn@outlook.com>
2025-09-14 20:31:08 +0300

    config: init integral-1.2.2

--- codegen.ts
+++ codegen.ts
@@ -3,9 +3,9 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: [
-    "https://api.studio.thegraph.com/query/50593/integral-v12/version/latest",
+    "https://api.studio.thegraph.com/query/82608/ve-analytics/v0.0.1",
     "https://api.studio.thegraph.com/query/50593/goerli-blocks/version/latest",
-    "https://api.studio.thegraph.com/query/50593/integral-v12-farming/version/latest",
+    "https://api.studio.thegraph.com/query/82608/ve-farms/v0.0.1",
   ],
   documents: "src/graphql/queries/!(*.d).{ts,tsx}",
   generates: {
