10e6e2d46249da5c62ff3391e17f4f402078b641
Kirill S <skdamn@outlook.com>
2025-09-22 14:53:01 +0300

    init integral 1.2.2

--- src/components/common/CurrencyLogo/index.tsx
+++ src/components/common/CurrencyLogo/index.tsx
@@ -2,100 +2,98 @@ import { Currency } from "@cryptoalgebra/custom-pools-and-sliding-fee-sdk";
 import React from "react";
 import { Address } from "wagmi";
 import EthLogo from "@/assets/tokens/ether.svg";
-import USDTLogo from "@/assets/tokens/usdt.png";
+import BTCLogo from "@/assets/tokens/wbtc.svg";
 import USDCLogo from "@/assets/tokens/usdc.svg";
+import EtherLogo from "@/assets/tokens/ether.svg";
+import ALGBLogo from "@/assets/algebra-logo.svg";
 import { cn } from "@/lib/utils";
 
 interface CurrencyLogoProps {
-  currency: Currency | undefined | null;
-  size: number;
-  className?: string;
-  style?: React.CSSProperties;
+    currency: Currency | undefined | null;
+    size: number;
+    className?: string;
+    style?: React.CSSProperties;
 }
 
 export const specialTokens: {
-  [key: Address]: { symbol: string; logo: string };
+    [key: Address]: { symbol: string; logo: string };
 } = {
-  ["0x94373a4919b3240d86ea41593d5eba789fef3848"]: {
-    symbol: "ETH",
-    logo: EthLogo,
-  },
-  ["0x7d98346b3b000c55904918e3d9e2fc3f94683b01"]: {
-    symbol: "USDT",
-    logo: USDTLogo,
-  },
-  ["0x42e87bddd77b0c4122b5dd4a8c62872610dcefef"]: {
-    symbol: "USDC",
-    logo: USDCLogo,
-  },
+    ["0x4200000000000000000000000000000000000006"]: {
+        symbol: "ETH",
+        logo: EtherLogo,
+    },
+    ["0xabac6f23fdf1313fc2e9c9244f666157ccd32990"]: {
+        symbol: "USDC",
+        logo: USDCLogo,
+    },
+    ["0xaff9ae92ef4362117d64fe51c20011a6ee456815"]: {
+        symbol: "BTC",
+        logo: BTCLogo,
+    },
+    ["0x253f3460bc16074b960f80421d72e6fa6ef786c8"]: {
+        symbol: "ALGB",
+        logo: ALGBLogo,
+    },
 };
 
-const CurrencyLogo = ({
-  currency,
-  size,
-  className,
-  style = {},
-}: CurrencyLogoProps) => {
-  if (!currency) return;
-  // if (!currency) return <Skeleton className={cn(`flex rounded-full bg-card`, className)} style={{ minWidth: `${size}px`, minHeight: `${size}px`, width: `${size}px`, height: `${size}px`, ...style }} />
+const CurrencyLogo = ({ currency, size, className, style = {} }: CurrencyLogoProps) => {
+    if (!currency) return;
+    // if (!currency) return <Skeleton className={cn(`flex rounded-full bg-card`, className)} style={{ minWidth: `${size}px`, minHeight: `${size}px`, width: `${size}px`, height: `${size}px`, ...style }} />
 
-  const address = currency.wrapped.address.toLowerCase() as Address;
+    const address = currency.wrapped.address.toLowerCase() as Address;
 
-  const classString = cn(
-    `w-[${size}px] h-[${size}px] min-w-[${size}px] min-h-[${size}px] bg-card-dark rounded-full`,
-    className
-  );
+    const classString = cn(`w-[${size}px] h-[${size}px] min-w-[${size}px] min-h-[${size}px] bg-card-dark rounded-full`, className);
 
-  if (address in specialTokens) {
+    if (address in specialTokens) {
+        return (
+            <img
+                src={specialTokens[address].logo}
+                alt={specialTokens[address].symbol}
+                width={size}
+                height={size}
+                className={classString}
+                style={{
+                    ...style,
+                    minWidth: `${size}px`,
+                    minHeight: `${size}px`,
+                    width: `${size}px`,
+                    height: `${size}px`,
+                }}
+            />
+        );
+    }
+
+    if (currency.isNative) {
+        return (
+            <img
+                src={EthLogo}
+                alt={"ETH"}
+                className={classString}
+                style={{
+                    ...style,
+                    minWidth: `${size}px`,
+                    minHeight: `${size}px`,
+                    width: `${size}px`,
+                    height: `${size}px`,
+                }}
+            />
+        );
+    }
+
     return (
-      <img
-        src={specialTokens[address].logo}
-        alt={specialTokens[address].symbol}
-        width={size}
-        height={size}
-        className={classString}
-        style={{
-          ...style,
-          minWidth: `${size}px`,
-          minHeight: `${size}px`,
-          width: `${size}px`,
-          height: `${size}px`,
-        }}
-      />
+        <div
+            className={`${classString} flex items-center justify-center bg-gray-200 text-black`}
+            style={{
+                minWidth: `${size}px`,
+                minHeight: `${size}px`,
+                width: `${size}px`,
+                height: `${size}px`,
+                ...style,
+            }}
+        >
+            {currency.symbol?.slice(0, 2)}
+        </div>
     );
-  }
-
-  if (currency.isNative) {
-    return (
-      <img
-        src={EthLogo}
-        alt={"ETH"}
-        className={classString}
-        style={{
-          ...style,
-          minWidth: `${size}px`,
-          minHeight: `${size}px`,
-          width: `${size}px`,
-          height: `${size}px`,
-        }}
-      />
-    );
-  }
-
-  return (
-    <div
-      className={`${classString} flex items-center justify-center bg-gray-200 text-black`}
-      style={{
-        minWidth: `${size}px`,
-        minHeight: `${size}px`,
-        width: `${size}px`,
-        height: `${size}px`,
-        ...style,
-      }}
-    >
-      {currency.symbol?.slice(0, 2)}
-    </div>
-  );
 };
 
 export default CurrencyLogo;
--- src/constants/custom-pool-deployer.ts
+++ src/constants/custom-pool-deployer.ts
@@ -1,8 +1,8 @@
 import { ADDRESS_ZERO } from "@cryptoalgebra/custom-pools-and-sliding-fee-sdk";
+import { ALGEBRA_STUB_PLUGIN } from "./addresses";
 
 export const CUSTOM_POOL_DEPLOYER_TITLES: { [key: string]: string } = {
-    "0x7e3387e0595552e992ede4476417704703866e5a": "HAVE PLUGIN",
-    "0xbb75acad36f08201a49a6dd077229d95f4e7bd50": "NO PLUGIN",
-    ["0xFD209C7e6b19131B2C36550950c66F0E4EbccfF0".toLowerCase()]: "All-Inclusive",
+    [ALGEBRA_STUB_PLUGIN.toLowerCase()]: "NO PLUGIN",
+    ["0x05d4bcB4940397C5fBA7C4E01a48Af882e84E214".toLowerCase()]: "All-Inclusive",
     [ADDRESS_ZERO]: "BASE",
 };
