eee65814399e84264d293f749d38afc4da619fca
lilchizh <xlilchizhx@gmail.com>
2025-11-07 13:25:27 +0300

    new generated

--- src/graphql/generated/graphql.tsx
+++ src/graphql/generated/graphql.tsx
@@ -504,6 +504,8 @@ export type Burn = {
   origin: Scalars['Bytes']['output'];
   owner?: Maybe<Scalars['Bytes']['output']>;
   pool: Pool;
+  reserves0: Scalars['BigDecimal']['output'];
+  reserves1: Scalars['BigDecimal']['output'];
   tickLower: Scalars['BigInt']['output'];
   tickUpper: Scalars['BigInt']['output'];
   timestamp: Scalars['BigInt']['output'];
@@ -640,6 +642,22 @@ export type Burn_Filter = {
   pool_not_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
   pool_starts_with?: InputMaybe<Scalars['String']['input']>;
   pool_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  reserves0?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_gt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_gte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves0_lt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_lte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_not?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_not_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves1?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_gt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_gte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves1_lt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_lte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_not?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_not_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
   tickLower?: InputMaybe<Scalars['BigInt']['input']>;
   tickLower_gt?: InputMaybe<Scalars['BigInt']['input']>;
   tickLower_gte?: InputMaybe<Scalars['BigInt']['input']>;
@@ -773,6 +791,8 @@ export enum Burn_OrderBy {
   PoolVolumeToken0 = 'pool__volumeToken0',
   PoolVolumeToken1 = 'pool__volumeToken1',
   PoolVolumeUsd = 'pool__volumeUSD',
+  Reserves0 = 'reserves0',
+  Reserves1 = 'reserves1',
   TickLower = 'tickLower',
   TickUpper = 'tickUpper',
   Timestamp = 'timestamp',
@@ -813,6 +833,7 @@ export enum Burn_OrderBy {
   TransactionGasLimit = 'transaction__gasLimit',
   TransactionGasPrice = 'transaction__gasPrice',
   TransactionId = 'transaction__id',
+  TransactionIndex = 'transaction__index',
   TransactionTimestamp = 'transaction__timestamp'
 }
 
@@ -1004,6 +1025,7 @@ export enum Collect_OrderBy {
   TransactionGasLimit = 'transaction__gasLimit',
   TransactionGasPrice = 'transaction__gasPrice',
   TransactionId = 'transaction__id',
+  TransactionIndex = 'transaction__index',
   TransactionTimestamp = 'transaction__timestamp'
 }
 
@@ -1014,7 +1036,6 @@ export type Deposit = {
   liquidity: Scalars['BigInt']['output'];
   owner: Scalars['Bytes']['output'];
   pool: Scalars['Bytes']['output'];
-  rangeLength: Scalars['BigInt']['output'];
 };
 
 export type Deposit_Filter = {
@@ -1068,14 +1089,6 @@ export type Deposit_Filter = {
   pool_not?: InputMaybe<Scalars['Bytes']['input']>;
   pool_not_contains?: InputMaybe<Scalars['Bytes']['input']>;
   pool_not_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
-  rangeLength?: InputMaybe<Scalars['BigInt']['input']>;
-  rangeLength_gt?: InputMaybe<Scalars['BigInt']['input']>;
-  rangeLength_gte?: InputMaybe<Scalars['BigInt']['input']>;
-  rangeLength_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
-  rangeLength_lt?: InputMaybe<Scalars['BigInt']['input']>;
-  rangeLength_lte?: InputMaybe<Scalars['BigInt']['input']>;
-  rangeLength_not?: InputMaybe<Scalars['BigInt']['input']>;
-  rangeLength_not_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
 };
 
 export enum Deposit_OrderBy {
@@ -1083,8 +1096,7 @@ export enum Deposit_OrderBy {
   Id = 'id',
   Liquidity = 'liquidity',
   Owner = 'owner',
-  Pool = 'pool',
-  RangeLength = 'rangeLength'
+  Pool = 'pool'
 }
 
 export type EternalFarming = {
@@ -1695,6 +1707,7 @@ export enum Flash_OrderBy {
   TransactionGasLimit = 'transaction__gasLimit',
   TransactionGasPrice = 'transaction__gasPrice',
   TransactionId = 'transaction__id',
+  TransactionIndex = 'transaction__index',
   TransactionTimestamp = 'transaction__timestamp'
 }
 
@@ -1709,6 +1722,8 @@ export type Mint = {
   origin: Scalars['Bytes']['output'];
   owner: Scalars['Bytes']['output'];
   pool: Pool;
+  reserves0: Scalars['BigDecimal']['output'];
+  reserves1: Scalars['BigDecimal']['output'];
   sender?: Maybe<Scalars['Bytes']['output']>;
   tickLower: Scalars['BigInt']['output'];
   tickUpper: Scalars['BigInt']['output'];
@@ -1812,6 +1827,22 @@ export type Mint_Filter = {
   pool_not_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
   pool_starts_with?: InputMaybe<Scalars['String']['input']>;
   pool_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  reserves0?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_gt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_gte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves0_lt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_lte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_not?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_not_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves1?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_gt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_gte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves1_lt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_lte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_not?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_not_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
   sender?: InputMaybe<Scalars['Bytes']['input']>;
   sender_contains?: InputMaybe<Scalars['Bytes']['input']>;
   sender_gt?: InputMaybe<Scalars['Bytes']['input']>;
@@ -1955,6 +1986,8 @@ export enum Mint_OrderBy {
   PoolVolumeToken0 = 'pool__volumeToken0',
   PoolVolumeToken1 = 'pool__volumeToken1',
   PoolVolumeUsd = 'pool__volumeUSD',
+  Reserves0 = 'reserves0',
+  Reserves1 = 'reserves1',
   Sender = 'sender',
   TickLower = 'tickLower',
   TickUpper = 'tickUpper',
@@ -1996,6 +2029,7 @@ export enum Mint_OrderBy {
   TransactionGasLimit = 'transaction__gasLimit',
   TransactionGasPrice = 'transaction__gasPrice',
   TransactionId = 'transaction__id',
+  TransactionIndex = 'transaction__index',
   TransactionTimestamp = 'transaction__timestamp'
 }
 
@@ -3668,9 +3702,46 @@ export enum PositionSnapshot_OrderBy {
   TransactionGasLimit = 'transaction__gasLimit',
   TransactionGasPrice = 'transaction__gasPrice',
   TransactionId = 'transaction__id',
+  TransactionIndex = 'transaction__index',
   TransactionTimestamp = 'transaction__timestamp',
   WithdrawnToken0 = 'withdrawnToken0',
   WithdrawnToken1 = 'withdrawnToken1'
+}
+
+export type PositionTransferCache = {
+  __typename?: 'PositionTransferCache';
+  id: Scalars['ID']['output'];
+  owner: Scalars['Bytes']['output'];
+};
+
+export type PositionTransferCache_Filter = {
+  /** Filter for the block changed event. */
+  _change_block?: InputMaybe<BlockChangedFilter>;
+  and?: InputMaybe<Array<InputMaybe<PositionTransferCache_Filter>>>;
+  id?: InputMaybe<Scalars['ID']['input']>;
+  id_gt?: InputMaybe<Scalars['ID']['input']>;
+  id_gte?: InputMaybe<Scalars['ID']['input']>;
+  id_in?: InputMaybe<Array<Scalars['ID']['input']>>;
+  id_lt?: InputMaybe<Scalars['ID']['input']>;
+  id_lte?: InputMaybe<Scalars['ID']['input']>;
+  id_not?: InputMaybe<Scalars['ID']['input']>;
+  id_not_in?: InputMaybe<Array<Scalars['ID']['input']>>;
+  or?: InputMaybe<Array<InputMaybe<PositionTransferCache_Filter>>>;
+  owner?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_gt?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_gte?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  owner_lt?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_lte?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_not?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_not_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_not_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+};
+
+export enum PositionTransferCache_OrderBy {
+  Id = 'id',
+  Owner = 'owner'
 }
 
 export type Position_Filter = {
@@ -4016,6 +4087,7 @@ export enum Position_OrderBy {
   TransactionGasLimit = 'transaction__gasLimit',
   TransactionGasPrice = 'transaction__gasPrice',
   TransactionId = 'transaction__id',
+  TransactionIndex = 'transaction__index',
   TransactionTimestamp = 'transaction__timestamp',
   WithdrawnToken0 = 'withdrawnToken0',
   WithdrawnToken1 = 'withdrawnToken1'
@@ -4066,6 +4138,8 @@ export type Query = {
   position?: Maybe<Position>;
   positionSnapshot?: Maybe<PositionSnapshot>;
   positionSnapshots: Array<PositionSnapshot>;
+  positionTransferCache?: Maybe<PositionTransferCache>;
+  positionTransferCaches: Array<PositionTransferCache>;
   positions: Array<Position>;
   reward?: Maybe<Reward>;
   rewards: Array<Reward>;
@@ -4460,6 +4534,24 @@ export type QueryPositionSnapshotsArgs = {
 };
 
 
+export type QueryPositionTransferCacheArgs = {
+  block?: InputMaybe<Block_Height>;
+  id: Scalars['ID']['input'];
+  subgraphError?: _SubgraphErrorPolicy_;
+};
+
+
+export type QueryPositionTransferCachesArgs = {
+  block?: InputMaybe<Block_Height>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<PositionTransferCache_OrderBy>;
+  orderDirection?: InputMaybe<OrderDirection>;
+  skip?: InputMaybe<Scalars['Int']['input']>;
+  subgraphError?: _SubgraphErrorPolicy_;
+  where?: InputMaybe<PositionTransferCache_Filter>;
+};
+
+
 export type QueryPositionsArgs = {
   block?: InputMaybe<Block_Height>;
   first?: InputMaybe<Scalars['Int']['input']>;
@@ -4735,6 +4827,8 @@ export type Subscription = {
   position?: Maybe<Position>;
   positionSnapshot?: Maybe<PositionSnapshot>;
   positionSnapshots: Array<PositionSnapshot>;
+  positionTransferCache?: Maybe<PositionTransferCache>;
+  positionTransferCaches: Array<PositionTransferCache>;
   positions: Array<Position>;
   reward?: Maybe<Reward>;
   rewards: Array<Reward>;
@@ -5129,6 +5223,24 @@ export type SubscriptionPositionSnapshotsArgs = {
 };
 
 
+export type SubscriptionPositionTransferCacheArgs = {
+  block?: InputMaybe<Block_Height>;
+  id: Scalars['ID']['input'];
+  subgraphError?: _SubgraphErrorPolicy_;
+};
+
+
+export type SubscriptionPositionTransferCachesArgs = {
+  block?: InputMaybe<Block_Height>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<PositionTransferCache_OrderBy>;
+  orderDirection?: InputMaybe<OrderDirection>;
+  skip?: InputMaybe<Scalars['Int']['input']>;
+  subgraphError?: _SubgraphErrorPolicy_;
+  where?: InputMaybe<PositionTransferCache_Filter>;
+};
+
+
 export type SubscriptionPositionsArgs = {
   block?: InputMaybe<Block_Height>;
   first?: InputMaybe<Scalars['Int']['input']>;
@@ -5313,6 +5425,8 @@ export type Swap = {
   pool: Pool;
   price: Scalars['BigInt']['output'];
   recipient: Scalars['Bytes']['output'];
+  reserves0: Scalars['BigDecimal']['output'];
+  reserves1: Scalars['BigDecimal']['output'];
   sender: Scalars['Bytes']['output'];
   tick: Scalars['BigInt']['output'];
   timestamp: Scalars['BigInt']['output'];
@@ -5467,6 +5581,22 @@ export type Swap_Filter = {
   recipient_not?: InputMaybe<Scalars['Bytes']['input']>;
   recipient_not_contains?: InputMaybe<Scalars['Bytes']['input']>;
   recipient_not_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  reserves0?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_gt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_gte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves0_lt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_lte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_not?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves0_not_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves1?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_gt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_gte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
+  reserves1_lt?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_lte?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_not?: InputMaybe<Scalars['BigDecimal']['input']>;
+  reserves1_not_in?: InputMaybe<Array<Scalars['BigDecimal']['input']>>;
   sender?: InputMaybe<Scalars['Bytes']['input']>;
   sender_contains?: InputMaybe<Scalars['Bytes']['input']>;
   sender_gt?: InputMaybe<Scalars['Bytes']['input']>;
@@ -5603,6 +5733,8 @@ export enum Swap_OrderBy {
   PoolVolumeUsd = 'pool__volumeUSD',
   Price = 'price',
   Recipient = 'recipient',
+  Reserves0 = 'reserves0',
+  Reserves1 = 'reserves1',
   Sender = 'sender',
   Tick = 'tick',
   Timestamp = 'timestamp',
@@ -5643,6 +5775,7 @@ export enum Swap_OrderBy {
   TransactionGasLimit = 'transaction__gasLimit',
   TransactionGasPrice = 'transaction__gasPrice',
   TransactionId = 'transaction__id',
+  TransactionIndex = 'transaction__index',
   TransactionTimestamp = 'transaction__timestamp'
 }
 
@@ -6692,6 +6825,7 @@ export type Transaction = {
   gasLimit: Scalars['BigInt']['output'];
   gasPrice: Scalars['BigInt']['output'];
   id: Scalars['ID']['output'];
+  index: Scalars['BigInt']['output'];
   mints: Array<Mint>;
   swaps: Array<Swap>;
   timestamp: Scalars['BigInt']['output'];
@@ -6781,6 +6915,14 @@ export type Transaction_Filter = {
   id_lte?: InputMaybe<Scalars['ID']['input']>;
   id_not?: InputMaybe<Scalars['ID']['input']>;
   id_not_in?: InputMaybe<Array<Scalars['ID']['input']>>;
+  index?: InputMaybe<Scalars['BigInt']['input']>;
+  index_gt?: InputMaybe<Scalars['BigInt']['input']>;
+  index_gte?: InputMaybe<Scalars['BigInt']['input']>;
+  index_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
+  index_lt?: InputMaybe<Scalars['BigInt']['input']>;
+  index_lte?: InputMaybe<Scalars['BigInt']['input']>;
+  index_not?: InputMaybe<Scalars['BigInt']['input']>;
+  index_not_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
   mints_?: InputMaybe<Mint_Filter>;
   or?: InputMaybe<Array<InputMaybe<Transaction_Filter>>>;
   swaps_?: InputMaybe<Swap_Filter>;
@@ -6802,6 +6944,7 @@ export enum Transaction_OrderBy {
   GasLimit = 'gasLimit',
   GasPrice = 'gasPrice',
   Id = 'id',
+  Index = 'index',
   Mints = 'mints',
   Swaps = 'swaps',
   Timestamp = 'timestamp'
