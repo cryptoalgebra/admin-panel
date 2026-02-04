45f1bf4230da9b66981e2b37447ecbce48c8fb0b
lilchizh <xlilchizhx@gmail.com>
2026-02-04 19:05:27 +0700

    change subgraphs

--- config/graphql-urls.ts
+++ config/graphql-urls.ts
@@ -1,9 +1,9 @@
 import { ChainId } from "@cryptoalgebra/integral-sdk";
 
 export const INFO_GRAPH_URL = {
-    [ChainId.BaseSepolia]: "https://api.studio.thegraph.com/query/82608/ve-analytics/version/latest",
+    [ChainId.BaseSepolia]: "https://api.goldsky.com/api/public/project_cm2cd1yfmmrav01u9b02f69vj/subgraphs/integral-ve-analytics/v1.0.0/gn",
 };
 
 export const FARMING_GRAPH_URL = {
-    [ChainId.BaseSepolia]: "https://api.studio.thegraph.com/query/82608/ve-farms/version/latest",
+    [ChainId.BaseSepolia]: "https://api.goldsky.com/api/public/project_cm2cd1yfmmrav01u9b02f69vj/subgraphs/integral-ve-farming/v1.0.0/gn",
 };
