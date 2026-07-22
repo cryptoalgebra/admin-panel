f709bbb8373e1b18e6394c7936157c0d627daa1d
damnnou <skdamn@outlook.com>
2026-07-22 18:56:35 +0300

    config: deploy on hemi

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -18,7 +18,6 @@ import {
     ALGEBRA_FACTORY,
     BINARY_LMSR_MARKET_MANAGER,
     FARMING_CENTER,
-    MULTICALL3,
     PLUGIN_FACTORY,
     SECURITY_REGISTRY,
     VOTER,
@@ -26,38 +25,38 @@ import {
 import { defineChain } from "viem";
 import { slidingFeePluginAbi } from "./abis/plugins/slidingFeePlugin";
 
-const baseSepoliaChain = /*#__PURE__*/ defineChain({
-    id: 84532,
-    network: "baseSepolia",
-    name: "Base Sepolia",
+const hemiChain = /*#__PURE__*/ defineChain({
+    id: 43111,
+    network: "hemi",
+    name: "Hemi",
     nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
     rpcUrls: {
         default: {
-            http: ["https://base-sepolia-rpc.publicnode.com"],
+            http: ["https://rpc.hemi.network/rpc"],
         },
         public: {
-            http: ["https://base-sepolia-rpc.publicnode.com"],
+            http: ["https://rpc.hemi.network/rpc"],
         },
     },
     blockExplorers: {
         default: {
-            name: "Basescan",
-            url: "https://sepolia.basescan.org",
+            name: "Hemi explorer",
+            url: "https://explorer.hemi.xyz",
         },
         etherscan: {
-            name: "Basescan",
-            url: "https://sepolia.basescan.org",
+            name: "Hemi explorer",
+            url: "https://explorer.hemi.xyz",
         },
     },
     contracts: {
         multicall3: {
-            address: MULTICALL3[84532],
+            address: "0x69D57B9D705eaD73a5d2f2476C30c55bD755cc2F",
+            blockCreated: 4904420,
         },
     },
 });
-
 /* configure supported networks here */
-export const wagmiNetworks: [AppKitNetwork, ...AppKitNetwork[]] = [baseSepoliaChain];
+export const wagmiNetworks: [AppKitNetwork, ...AppKitNetwork[]] = [hemiChain];
 
 const rawContracts = [
     { name: "AlgebraFactory", abi: algebraFactoryABI },
